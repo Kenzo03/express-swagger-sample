962385ad21249734f9d3a5cdba1f5ef472da8513
kenzo03 <kevinwijaya480@gmail.com>
2022-08-27 22:07:34 +0700

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express"),
-  bodyParser = require("body-parser");
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
+
+require('dotenv').config()
 
 const app = express();
 app.use(
@@ -12,6 +16,41 @@ app.use(bodyParser.json());
 app.use("/books", require("./routes/books"));
 
 const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV == "dev") {
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "LogRocket Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/books",
+        },
+      ],
+    },
+    apis: ["./routes/books.js"],
+  };
+
+  const specs = swaggerJsdoc(options);
+
+
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+  );
+}
+
+
 app.listen(PORT);
 
 console.debug("Server listening on port: " + PORT);
